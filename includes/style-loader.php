c324f075f0592db32a9821265a08e1b0614365cd
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-12-09 03:21:41 +0800

    cleaned some css

--- includes/style-loader.php
+++ includes/style-loader.php
@@ -6,28 +6,23 @@ $currentPage = basename($_SERVER['PHP_SELF'],".php");
 $satoshi            = '<link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"/>';
 $source_serif       = '<link href="https://fonts.cdnfonts.com/css/source-serif-pro" rel="stylesheet"/>';
 
-// css
-$root               = '<link href="./assets/styles/global/root.css" rel="stylesheet"/>';
 $global             = '<link href="./assets/styles/global/global.css" rel="stylesheet"/>';
-$header             = '<link href="./assets/styles/components/header.css" rel="stylesheet"/>';
-$footer             = '<link href="./assets/styles/components/footer.css" rel="stylesheet"/>';
-$productCard        = '<link href="./assets/styles/components/product-card.css" rel="stylesheet"/>';
+$cards        = '<link href="./assets/styles/components/cards.css" rel="stylesheet"/>';
 $productCarousel    = '<link href="./assets/styles/components/product-carousel.css" rel="stylesheet"/>';
 $actionOverlay      = '<link href="./assets/styles/components/action-menu.css" rel="stylesheet"/>';
 $forms              = '<link href="./assets/styles/components/forms.css" rel="stylesheet"/>';
 
-$home               = '<link href="./assets/styles/home.css" rel="stylesheet"/>';
-$shop               = '<link href="./assets/styles/shop.css" rel="stylesheet"/>';
-$productPage        = '<link href="./assets/styles/product.css" rel="stylesheet"/>';
-$summary            = '<link href="./assets/styles/summary.css" rel="stylesheet"/>';
-$reservations       = '<link href="./assets/styles/reservations.css" rel="stylesheet"/>';
-$login              = '<link href="./assets/styles/login.css" rel="stylesheet"/>';
-$review             = '<link href="./assets/styles/review.css" rel="stylesheet"/>';
-$manage             = '<link href="./assets/styles/manage.css" rel="stylesheet"/>';
+$home               = '<link href="./assets/styles/pages/home.css" rel="stylesheet"/>';
+$shop               = '<link href="./assets/styles/pages/shop.css" rel="stylesheet"/>';
+$productPage        = '<link href="./assets/styles/pages/product.css" rel="stylesheet"/>';
+$summary            = '<link href="./assets/styles/pages/summary.css" rel="stylesheet"/>';
+$reservations       = '<link href="./assets/styles/pages/reservations.css" rel="stylesheet"/>';
+$login              = '<link href="./assets/styles/pages/login.css" rel="stylesheet"/>';
+$review             = '<link href="./assets/styles/pages/review.css" rel="stylesheet"/>';
+$manage             = '<link href="./assets/styles/pages/manage.css" rel="stylesheet"/>';
 
 echo 
-$satoshi, $source_serif, $root, $global, $header, $footer, 
-$productCard, $productCarousel, $actionOverlay, $forms;
+$satoshi, $source_serif, $global, $cards, $productCarousel, $actionOverlay, $forms;
 
 if($currentPage == 'index') {
     echo $home;
