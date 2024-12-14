f0923b881d3dbe2df9f7ad73548e99c5e11706be
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-12-14 15:53:41 +0800

    fix label for attribute

--- includes/components/auth.php
+++ includes/components/auth.php
@@ -7,12 +7,12 @@
         </header>
         <form action="./handlers/auth/login.php" method="POST" class="auth__form">
             <div class="auth__field-group">
-                <input type="text" class="auth__field"  name="login-email" autocomplete="off" placeholder="-"/>
                 <label for="login-email" class="auth__label">Email</label>
+                <input type="text" id="login-email" class="auth__field"  name="login-email" autocomplete="off" placeholder="-"/>
             </div>
             <div class="auth__field-group">
-                <input type="password" class="auth__field"  name="login-password" autocomplete="off" placeholder="-"/>
                 <label for="login-password" class="auth__label">Password</label>
+                <input type="password" id="login-password" class="auth__field"  name="login-password" autocomplete="off" placeholder="-"/>
             </div>
             <button type="submit" class="button button--filled-dark auth__submit">Login</button>
         </form>
@@ -28,20 +28,20 @@
         </header>
         <form action="./handlers/auth/register.php" method="POST" class="auth__form">
             <div class="auth__field-group">
-                <input type="text" class="auth__field" name="register-fname" autocomplete="off" required/>
                 <label for="register-fname" class="auth__label">First Name</label>
+                <input type="text" id="register-fname" class="auth__field" name="register-fname" autocomplete="off" required/>
             </div>
             <div class="auth__field-group">
-                <input type="text" class="auth__field"  name="register-lname" autocomplete="off" required/>
                 <label for="register-lname" class="auth__label">Last Name</label>
+                <input type="text" id="register-lname" class="auth__field"  name="register-lname" autocomplete="off" required/>
             </div>
             <div class="auth__field-group">
-                <input type="email" class="auth__field" name="register-email" autocomplete="off" required/>
                 <label for="register-email" class="auth__label">Email</label>
+                <input type="email" id="register-email" class="auth__field" name="register-email" autocomplete="off" required/>
             </div>
             <div class="auth__field-group">
-                <input type="password" class="auth__field" name="register-password"autocomplete="off" required/>
                 <label for="register-password" class="auth__label">Password</label>
+                <input type="password" id="register-password" class="auth__field" name="register-password"autocomplete="off" required/>
             </div>
             <button type="submit" class="button button--filled-dark auth__submit">Create Account</button>
         </form>
--- login.php
+++ login.php
@@ -47,12 +47,12 @@ if(isset($_SESSION['login_success']) && $_SESSION['login_success'] == false) {
                     </header>
                     <form action="./handlers/auth/login.php" method="POST" class="auth__form">
                         <div class="auth__field-group">
-                            <input type="text" class="auth__field" name="login-email" autocomplete="off"/>
                             <label for="login-email" class="auth__label">Email</label>
+                            <input type="text" id="login-email" class="auth__field" name="login-email" autocomplete="off"/>
                         </div>
                         <div class="auth__field-group">
-                            <input type="password" class="auth__field" name="login-password" autocomplete="off"/>
                             <label for="login-password" class="auth__label">Password</label>
+                            <input type="password" id="login-password" class="auth__field" name="login-password" autocomplete="off"/>
                         </div>
                         <button type="submit" class="button button--filled-dark auth__submit">Login</button>
                     </form>
@@ -68,20 +68,20 @@ if(isset($_SESSION['login_success']) && $_SESSION['login_success'] == false) {
                     </header>
                     <form action="./handlers/auth/register.php" method="POST" class="auth__form">
                         <div class="auth__field-group">
-                            <input type="text" class="auth__field" name="register-fname" autocomplete="off" required/>
                             <label for="register-fname" class="auth__label">First Name</label>
+                            <input type="text" id="register-fname" class="auth__field" name="register-fname" autocomplete="off" required/>
                         </div>
                         <div class="auth__field-group">
-                            <input type="text" class="auth__field" name="register-lname" autocomplete="off" required/>
                             <label for="register-lname" class="auth__label">Last Name</label>
+                            <input type="text" id="register-lname" class="auth__field" name="register-lname" autocomplete="off" required/>
                         </div>
                         <div class="auth__field-group">
-                            <input type="text" class="auth__field" name="register-email" autocomplete="off" required>
-                            <label for="register-email" class="auth__label">Email</label>
+                            <label for="register-email"  class="auth__label">Email</label>
+                            <input type="text" id="register-email" class="auth__field" name="register-email" autocomplete="off" required>
                         </div>
                         <div class="auth__field-group">
-                            <input type="password" class="auth__field" name="register-password"autocomplete="off" required/>
-                            <label for="register-password" class="auth__label">Password</label>
+                            <label for="register-password"  class="auth__label">Password</label>
+                            <input type="password" id="register-password" class="auth__field" name="register-password"autocomplete="off" required/>
                         </div>
                         <button type="submit" class="button button--filled-dark auth__submit">Create Account</button>
                     </form>
