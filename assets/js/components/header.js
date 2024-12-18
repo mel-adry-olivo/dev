aab2c5f801cc5f12095bf4c1d42d9d1231f68f54
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2024-12-18 12:33:56 +0800

    remove header hiding

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -3,9 +3,6 @@ import { addClass, removeClass } from '../utils/dom.js';
 const pageOverlay = document.querySelector('.page-overlay-nav');
 const nav = document.querySelector('.header__nav-list');
 const header = document.querySelector('.header__wrapper');
-let lastScrollY = 0;
-let isScrolling = false;
-const throttleDelay = 120; // ms
 
 function initHeader() {
     window.addEventListener('resize', () => handleResize());
@@ -23,33 +20,12 @@ function initHeader() {
             toggleNav();
         }
     });
-
-    window.addEventListener('scroll', () => {
-        if (!isScrolling) {
-            handleScroll();
-            isScrolling = true;
-
-            setTimeout(() => {
-                isScrolling = false;
-            }, throttleDelay);
-        }
-    });
 }
 
 function handleResize() {
     if (pageOverlay.classList.contains('show')) {
         toggleNav();
     }
-}
-
-function handleScroll() {
-    const currentScroll = window.scrollY;
-
-    currentScroll > lastScrollY
-        ? addClass(header, 'hidden')
-        : removeClass(header, 'hidden');
-
-    lastScrollY = currentScroll;
 }
 
 function toggleNav() {
